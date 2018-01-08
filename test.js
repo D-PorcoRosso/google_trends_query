42948cbfa872fdf46181f0913e2f069728273c6f
Daniel <shechiway@gmail.com>
2018-01-08 11:58:01 +0800

    remove useless file

--- test.js
+++
@@ -1,9 +0,0 @@
-const googleTrends = require('google-trends-api')
-
-googleTrends.relatedQueries({keyword: 'Vulnerability', startTime: new Date('2017-10-10'), endTime: new Date('2017-10-16')})
-.then((res) => {
-  console.log(res);
-})
-.catch((err) => {
-  console.log(err);
-})
