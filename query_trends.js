ecfccb3d4d1d012c1c039442fef0ecc51422815a
Daniel <shechiway@gmail.com>
2018-01-19 17:58:47 +0800

    parse result

--- query_trends.js
+++ query_trends.js
@@ -8,7 +8,28 @@ console.log(key,startTime,endTime)
 
 googleTrends.relatedQueries({keyword: key, startTime: new Date(startTime), endTime: new Date(endTime)})
 .then((res) => {
-  console.log(res);
+  var result = {};
+  var obj = JSON.parse(res);
+  var rankedList = obj['default']['rankedList'];
+
+  rankedList.forEach(
+    function(list){
+        var test = list['rankedKeyword']
+
+        test.forEach(
+            function(content){
+                var temp = content['query'].split(' ')
+                temp.forEach(function(query_key){
+                    if ( query_key.toLowerCase() !== key.toLowerCase() ) {
+                        if ( query_key in result )
+                            result[query_key]++
+                        else
+                            result[query_key] = 1
+                    }
+                })
+            });
+    });
+  console.log(result)
 })
 .catch((err) => {
   console.log(err);
